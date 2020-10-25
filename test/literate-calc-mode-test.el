c253932769e58304f138b4e7d03dd7784bf2f97e
Robin Schroer <sulami@peerwire.org>
2020-10-25 15:57:02 +0100

    Add a commentary for the tests to appease the linter

--- test/literate-calc-mode-test.el
+++ test/literate-calc-mode-test.el
@@ -15,6 +15,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; Tests for literate-calc-mode.
+
 ;;; Code:
 
 (require 'ert)
